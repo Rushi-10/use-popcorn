1296ff45bdf7eb4e6625c72f2ea22ecc8478b67a
rushi10 <rushimadhure10@gmail.com>
2024-07-03 19:42:21 +0530

    updating with using custom hook useMovies

--- src/App v2.js
+++ src/App v2.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
+import {useMovies } from "./useMovies";
 
 const tempMovieData = [
   {
@@ -50,17 +51,13 @@ const tempWatchedData = [
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
-const key="f84fc31d";
-
+ const key="f84fc31d";
 export default function App() {
   const [query, setQuery] = useState("inception");
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+  const{movies,isLoading,error}=useMovies(query,onCloseMovie);
   const [watched, setWatched] = useState(function () {
     const storedData = localStorage.getItem("watched");
-
     return JSON.parse(storedData);
   });
 
@@ -72,45 +69,7 @@ export default function App() {
     [watched]
   );
 
-  useEffect(
-    function () {
-      const controller = new AbortController();
-      async function fetchMovies() {
-        try {
-          setIsLoading(true);
-          const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${key}&
-s=${query}`,
-            { signal: controller.signal }
-          );
-
-          if (!res.ok) throw new Error("Something wrong with fetching movies");
-          const data = await res.json();
-
-          if (data.Response === "False") throw new Error("Movie Not Found");
-          setMovies(data.Search);
-          setError("");
-        } catch (err) {
-          if (err.name !== "AbortError") {
-            setError(err.message);
-          }
-        } finally {
-          setIsLoading(false);
-        }
-      }
-      if (query.length < 3) {
-        setMovies([]);
-        setError("");
-        return;
-      }
-      onCloseMovie();
-      fetchMovies();
-      return function () {
-        controller.abort();
-      };
-    },
-    [query]
-  );
+  
 
   function onSelectMovie(id) {
     setSelectedId((selectedId) => (selectedId === id ? null : id));
